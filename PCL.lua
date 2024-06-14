4f7209ca5898f5ea324445e62c0702c0ca259423
Main <fernandoveigaaguiar1@gmail.com>
2024-06-14 19:05:44 -0300

    Update PCL.lua

--- PCL.lua
+++ PCL.lua
@@ -1,5 +1,5 @@
 repeat
   wait()
 until game:IsLoaded()
-
+setclipboard("https://github.com/nseinkkk")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nseinkkk/universal-script/main/PCL"))()
