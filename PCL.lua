c6ae0b9da6da6eb6c8fbbb8f8ef1eff5de78dd79
Main <fernandoveigaaguiar1@gmail.com>
2024-06-14 10:40:35 -0300

    Update PCL.lua

--- PCL.lua
+++ PCL.lua
@@ -3,4 +3,5 @@ repeat
 until game:IsLoaded()
 setclipboard("https://discord.com/invite/g3fEe7D5")
 local version = 10.75
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nseinkkk/LuaEncoder/main/updater.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nseinkkk/universal-script/main/PCL"))()
